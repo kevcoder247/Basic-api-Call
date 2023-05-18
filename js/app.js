35708f5db0d5ecdaab55903311460478c7ea9d76
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-05-18 15:20:58 -0400

    user can now input a movie and view a movie

--- js/app.js
+++ js/app.js
@@ -1,21 +1,38 @@
+let movieData, userInput;
+
 const $title = $('#title');
 const $year = $('#year');
 const $rated = $('#rated');
 const $img = $('#img')
 const $boxOffice = $('#box-office');
+const $input = $('input[type="text"]');
 
-$.ajax({
-  url: 'https://www.omdbapi.com/?apikey=53aa2cd6&t=Frozen'
-}).then(
-  (data) => {
-    $title.text(data.Title);
-    $img.attr('src', data.Poster)
-    $year.text(data.Year);
-    $rated.text(data.Rated);
-    $boxOffice.text(data.BoxOffice)
-    console.log(data)
-  },
-  (error) => {
-    console.log('bad request', error);
+
+$('form').on('submit', handleGetData);
+
+function handleGetData(event){
+  // calling preventDefault() on a 'submit' event will prevent a page refresh 
+  event.preventDefault();
+  
+  // getting the user input
+  userInput = $input.val();
+  $.ajax({
+    url: 'https://www.omdbapi.com/?apikey=53aa2cd6&t=' + userInput
+  }).then(
+    (data) => {
+     movieData = data;
+     render(); 
+    },
+    (error) => {
+      console.log('bad request', error);
+    }
+  )
+
+  function render(){
+    $title.text(movieData.Title);
+    $year.text(movieData.Year);
+    $rated.text(movieData.Rated)
+    $img.attr('src', movieData.Poster)
   }
-)
+
+}
